907a991f50fb3aa78723cddd35cc92bd6616d587
Vitor Kravszenko <vitor.kravs@evolutionsoft.com.br>
2025-04-26 12:58:06 -0300

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Vitor Kravszenko: Desenvolvedor Front-End | Soluções Inovadoras",
+  description: "Bem-vindo ao meu portfólio! Aqui você conhecerá um pouco mais sobre mim, minha trajetória, projetos dos quais participei e minhas habilidades como desenvolvedor. Espero que você tenha uma ótima experiência aqui.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${roboto.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+
+    </main>
+  );
+}
