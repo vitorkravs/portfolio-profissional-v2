4e6907b42987c4a28ae9873868f4a4a64c2788f8
vitorkravs <vitorkravs3@gmail.com>
2025-04-29 00:33:22 -0300

    fix: trava de segurança ao acessar document na função changeTheme em utils

--- utils/changeTheme.ts
+++ utils/changeTheme.ts
@@ -1,32 +1,18 @@
 export function changeTheme(color: "cyan" | "orange" | "violet" | "emerald") {
+    if (typeof document === "undefined") return;
+
     switch (color) {
         case "cyan":
-            document.documentElement.classList.remove(
-                "orange",
-                "violet",
-                "emerald"
-            );
+            document.documentElement.classList.remove("orange", "violet", "emerald");
             break;
         case "orange":
-            document.documentElement.classList.remove(
-                "cyan",
-                "violet",
-                "emerald"
-            );
+            document.documentElement.classList.remove("cyan", "violet", "emerald");
             break;
         case "violet":
-            document.documentElement.classList.remove(
-                "orange",
-                "cyan",
-                "emerald"
-            );
+            document.documentElement.classList.remove("orange", "cyan", "emerald");
             break;
         case "emerald":
-            document.documentElement.classList.remove(
-                "orange",
-                "violet",
-                "cyan"
-            );
+            document.documentElement.classList.remove("orange", "violet", "cyan");
             break;
     }
 }
