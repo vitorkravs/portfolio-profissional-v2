7aa33941e9d8320908cde90f2efc1d63bbd63d52
vitorkravs <vitorkravs3@gmail.com>
2025-04-29 22:35:50 -0300

    fix: importar component navbar com dynamic do next para resolver problema de build vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
-import Navbar from "@/components/Navbar";
+import dynamic from "next/dynamic";
+
+const Navbar = dynamic(() => import("@/components/Navbar"), { ssr: false })
 
 export default function Home() {
   return (
