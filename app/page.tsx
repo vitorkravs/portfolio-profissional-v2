25270d7cde5957b9dd1f22c1c42a0ab87a93ad75
vitorkravs <vitorkravs3@gmail.com>
2025-05-23 00:17:28 -0300

    fix: corrigir erro de build de ReferenceError: document is not defined

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-"use client"
-import Header from "@/components/Header";
+"use client";
 
+import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [isMounted, setIsMounted] = useState(false);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    if (typeof document !== 'undefined') {
-      setIsMounted(true);
-    }
+    setIsClient(true);
   }, []);
 
-  if (!isMounted) return null;
+  if (!isClient) {
+    return null;
+  }
 
   return (
     <>
