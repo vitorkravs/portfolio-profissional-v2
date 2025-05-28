f019c6787aff8312e11dba601894a7ec7a46b0a3
vitorkravs <vitorkravs3@gmail.com>
2025-05-27 22:47:52 -0300

    feat: retirar animação de scroll da navbar e adicionar o componente AnimationScroll e ScrollToTopButton na page principal

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 import dynamic from 'next/dynamic';
 import { useEffect, useState } from "react";
 
+import AnimationScroll from '@/components/AnimationScroll';
+import ScrollToTopButton from '@/components/ScrollToTopButton';
+
 const Navbar = dynamic(() => import("@/components/Navbar"), { ssr: false });
 const Header = dynamic(() => import("@/components/Header"), { ssr: false });
 
@@ -22,11 +25,16 @@ export default function Home() {
       <div className="lg:mx-16">
         <Navbar />
       </div>
-      <div className="w-full flex justify-center items-center">
+      <div className="relative w-full flex justify-center items-center">
         <Header />
+        <AnimationScroll />
       </div>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start ">
+        <div className='h-500'></div>
+        <div className='h-500'></div>
       </main>
+
+      <ScrollToTopButton />
     </>
   );
 }
