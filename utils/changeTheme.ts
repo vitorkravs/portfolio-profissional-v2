1c5e0e9ddcc9a6bfa69f1e7a01c0932b2ecabf49
vitorkravs <vitorkravs3@gmail.com>
2025-09-21 21:42:55 -0300

    fix: adicionar cor na classe ao utilizar a função changeTheme

--- utils/changeTheme.ts
+++ utils/changeTheme.ts
@@ -4,15 +4,19 @@ export function changeTheme(color: "cyan" | "orange" | "violet" | "emerald") {
     switch (color) {
         case "cyan":
             document.documentElement.classList.remove("orange", "violet", "emerald");
+            document.documentElement.classList.add('cyan');
             break;
         case "orange":
             document.documentElement.classList.remove("cyan", "violet", "emerald");
+            document.documentElement.classList.add('orange');
             break;
         case "violet":
             document.documentElement.classList.remove("orange", "cyan", "emerald");
+            document.documentElement.classList.add('violet');
             break;
         case "emerald":
             document.documentElement.classList.remove("orange", "violet", "cyan");
+            document.documentElement.classList.add('emerald');
             break;
     }
 }
